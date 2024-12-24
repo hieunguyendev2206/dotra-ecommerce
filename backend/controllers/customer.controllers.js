cacdd9387e4382cafe5b58e3227ba74f0ece653c
HieuNguyenDev <2100011408@nttu.edu.vn>
2024-12-25 01:25:31 +0700

    update https_v5

--- backend/controllers/customer.controllers.js
+++ backend/controllers/customer.controllers.js
@@ -433,9 +433,6 @@ class customerController {
                     });
                     res.cookie("customer_access_token", customer_access_token, {
                         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                        httpOnly: true,
-                        secure: true, // Chỉ cho phép trên HTTPS
-                        sameSite: "none", // Cho phép sử dụng trên các domain khác nhau
                     });
                     const urlRedirect = `${env.CLIENT_REDIRECT_CALLBACK}?customer_access_token=${customer_access_token}`;
                     return res.redirect(urlRedirect);
@@ -460,9 +457,6 @@ class customerController {
                     });
                     res.cookie("customer_access_token", customer_access_token, {
                         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                        httpOnly: true,
-                        secure: true, // Chỉ cho phép trên HTTPS
-                        sameSite: "none", // Cho phép sử dụng trên các domain khác nhau
                     });
                     const urlRedirect = `${env.CLIENT_REDIRECT_CALLBACK}?customer_access_token=${customer_access_token}`;
                     return res.redirect(urlRedirect);
