1f2bc4d667f5150147e7a810f145803aa03fbf64
HieuNguyenDev <114110346+hieunguyendev2206@users.noreply.github.com>
2024-12-25 00:07:15 +0700

    Update tailwind.config.js

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
