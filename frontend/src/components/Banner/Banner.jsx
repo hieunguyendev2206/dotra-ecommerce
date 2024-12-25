43eed755b1f6829db7abb3379e94fad1e984a3ec
HieuNguyenDev <2100011408@nttu.edu.vn>
2024-12-25 11:59:35 +0700

    update https_v9

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -1,6 +1,15 @@
 import Carousel from "react-multi-carousel";
 import {Link} from "react-router-dom";
 import "react-multi-carousel/lib/styles.css";
+import banner1 from "../../assets/banners/1.png";
+import banner2 from "../../assets/banners/2.png";
+import banner3 from "../../assets/banners/3.png";
+import banner4 from "../../assets/banners/4.png";
+import banner5 from "../../assets/banners/5.png";
+import banner6 from "../../assets/banners/6.png";
+import banner7 from "../../assets/banners/7.png";
+import banner8 from "../../assets/banners/8.png";
+
 
 const Banner = () => {
     const responsive = {
@@ -21,6 +30,9 @@ const Banner = () => {
             items: 1,
         },
     };
+
+    const banners = [banner1, banner2, banner3, banner4, banner5, banner6, banner7, banner8];
+
     return (
         <div className="w-full md-lg:mt-6">
             <div className="w-[85%] lg:w-[90%] mx-auto">
@@ -34,17 +46,21 @@ const Banner = () => {
                                 showDots={true}
                                 responsive={responsive}
                             >
-                                {[1, 2, 3, 4, 5, 6, 7, 8].map((img, i) => (
+                                {banners.map((img, i) => (
                                     <Link
                                         className="lg-md:h-[440px] h-auto w-full block rounded-md"
                                         key={i}
                                         to="#"
                                     >
-                                        <img src={`/src/assets/banners/${img}.png`} alt=""
-                                             className="object-cover w-full h-auto rounded-md"/>
+                                        <img
+                                            src={img}
+                                            alt={`Banner ${i + 1}`}
+                                            className="object-cover w-full h-auto rounded-md"
+                                        />
                                     </Link>
                                 ))}
                             </Carousel>
+
                         </div>
                     </div>
                 </div>
